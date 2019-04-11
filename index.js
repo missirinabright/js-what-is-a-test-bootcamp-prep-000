179c877c18de0c4a59d4c8b0f76dea0702621077
Irina Bright <missirinabright@gmail.com>
2019-04-11 21:52:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 // write your code below!
-var index = require("../index.js");
-describe("Message", () => {
-    it("gives the name and height", () => {
-      expect(index.message).toInclude(index.name);
-      expect(index.message).toInclude(index.height);
-    });
-  });
-  
-  console.log(message);
